4b045b58c47bdc973efce2700bace5d37bad553a
Piotr Zielinski <xenox40@gmail.com>
2019-04-15 15:09:35 +0200

    init commit

---
+++ desktop/src/com/geometric/wars/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.geometric.wars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.geometric.wars.GeometricWars;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GeometricWars(), config);
+	}
+}

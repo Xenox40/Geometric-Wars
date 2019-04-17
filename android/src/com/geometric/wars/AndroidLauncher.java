52956368bbe7a6d59b901a2c0e35797688f72a23
Piotr Zieliński <xenox40@gmail.com>
2019-04-17 20:09:07 +0200

    Revert " Add Not-interactive environment objects AB#17"

--- android/src/com/geometric/wars/AndroidLauncher.java
+++ android/src/com/geometric/wars/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.geometric.wars.GeometricWars;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override

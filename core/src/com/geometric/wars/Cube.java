6a0888a43a42cad9457fa7b9b86d16aea31822d1
ggawryal <ggawryal@github.com>
2019-04-18 01:03:36 +0200

    added rotation animation

--- core/src/com/geometric/wars/Cube.java
+++ core/src/com/geometric/wars/Cube.java
@@ -5,14 +5,47 @@ import com.badlogic.gdx.math.Vector3;
 public class Cube {
 
     private CubeView cubeView;
-    private Vector3 md = new Vector3(0,0,0);
-    private Vector3 mr = new Vector3(0,0,0);
-    static final float movementTimeInSeconds = 0.8f;
-    int px = 0, pz = 0;
+    static final float movementTimeInSeconds = 0.5f;
+
+    private float rotationAngleSumInDegrees;
+    private boolean moving = false;
+    private int rotationDirection;
+
+    public boolean isMoving() {
+        return moving;
+    }
+    private void startRotating() {
+        moving = true;
+        rotationAngleSumInDegrees = 0;
+    }
+
+    private int bx = 0, bz = 0;
+    private int ax = 0, az = 0;
+
 
     public Cube(CubeView cubeView) {
         this.cubeView = cubeView;
     }
+
+
+    public void updateRotationAndPosition() {
+        if(isMoving()) {
+            float deltaDegrees = (float)(Gdx.graphics.getDeltaTime()*90/(double)(movementTimeInSeconds));
+            rotationAngleSumInDegrees += deltaDegrees;
+            if(rotationAngleSumInDegrees >= 90){
+                deltaDegrees = 90-rotationAngleSumInDegrees;
+                rotationAngleSumInDegrees = 90;
+                moving = false;
+            }
+            cubeView.rotate(rotationDirection, deltaDegrees,bx,bz);
+            if(!moving) {
+                rotationAngleSumInDegrees = 0;
+                bx = ax;
+                bz = az;
+            }
+        }
+    }
+
 
     /**
      *
@@ -32,31 +65,36 @@ public class Cube {
     }
 
     public void moveUp(){
-        md.set(1,0,0);
-        mr.set(-1,0,0);
-        pz--;
-        cubeView.rotate(md,mr,90);
+        if(isMoving())
+            return;
+        startRotating();
+        rotationDirection = 0;
+        az--;
     }
 
     public void moveRight(){
-        md.set(1,0,0);
-        mr.set(0,0,-1);
-        px++;
-        cubeView.rotate(md,mr,90);
+        if(isMoving())
+            return;
+        startRotating();
+        rotationDirection = 1;
+        ax++;
     }
 
     public void moveDown(){
-        md.set(0, 0, 1);
-        mr.set(1,0,0);
-        pz++;
-        cubeView.rotate(md,mr,90);
-    }
-    
-    public void moveLeft(){
-        md.set(0, 0,1);
-        mr.set(0,0,1);
-        px--;
-        cubeView.rotate(md,mr,90);
+        if(isMoving())
+            return;
+        startRotating();
+
+        rotationDirection = 2;
+        az++;
     }
 
+    public void moveLeft(){
+        if(isMoving())
+            return;
+        startRotating();
+
+        rotationDirection = 3;
+        ax--;
+    }
 }
--- core/src/com/geometric/wars/CubeView.java
+++ core/src/com/geometric/wars/CubeView.java
@@ -17,7 +17,7 @@ import com.badlogic.gdx.math.Vector3;
 public class CubeView {
     Model model;
     ModelInstance cubeModelInstance;
-    private final float size = 1;
+    private final float size = 1f;
     private KeyInputListener arrowAction;
     CubeView(){
         ModelBuilder modelBuilder = new ModelBuilder();
@@ -27,32 +27,66 @@ public class CubeView {
         cubeModelInstance = new ModelInstance(model);
     }
 
-    public void rotate(Vector3 md, Vector3 mr, float degrees){
 
-        Vector3 translationVector = new Vector3(-size / 2 + md.x, -size / 2 + md.y, -size / 2 + md.z);
+    private void rotate(Vector3 translation, Vector3 rotation, float degrees, Vector3 pp) {
+
+        Vector3 curPosition  = cubeModelInstance.transform.getTranslation(new Vector3());
 
         Quaternion curRotation = cubeModelInstance.transform.getRotation(new Quaternion());
+
+
         Quaternion invRotation = new Quaternion(-curRotation.x / curRotation.len2(), -curRotation.y / curRotation.len2(),
                 -curRotation.z / curRotation.len2(),  curRotation.w / curRotation.len2());
 
         cubeModelInstance.transform
                 .rotate(invRotation)
-                .translate(translationVector.cpy().scl(1))
-                .rotate(mr, degrees)
-                .translate(translationVector.cpy().scl(-1))
-                .rotate(curRotation);
+                .translate(curPosition.cpy().sub(pp).scl(-1))
+                .translate(translation.cpy().scl(1))
+                .rotate(rotation, degrees)
+                .translate(translation.cpy().scl(-1))
+                .translate(curPosition.cpy().sub(pp).scl(1))
+                .rotate(curRotation)
+            ;
     }
+
+    public void rotateRight(float degrees,Vector3 pp) {
+        rotate(new Vector3(size/2,-size/2,0), new Vector3(0,0,-1),degrees,pp);
+    }
+
+    public void rotateLeft(float degrees,Vector3 pp) {
+        rotate(new Vector3(-size/2,-size/2,0), new Vector3(0,0,1),degrees,pp);
+    }
+
+    public void rotateUp(float degrees,Vector3 pp) {
+        rotate(new Vector3(0,-size/2,-size/2), new Vector3(-1,0,0),degrees,pp);
+    }
+
+    public void rotateDown(float degrees,Vector3 pp) {
+        rotate(new Vector3(0,-size/2,size/2), new Vector3(1,0,0),degrees,pp);
+    }
+
+    public void rotate(int direction,float degrees,int px, int pz) {
+        if(direction == 0)
+            rotateUp(degrees,new Vector3(px,0,pz));
+        else if(direction == 1)
+            rotateRight(degrees,new Vector3(px,0,pz));
+        else if(direction == 2)
+            rotateDown(degrees,new Vector3(px,0,pz));
+        else if(direction == 3)
+            rotateLeft(degrees,new Vector3(px,0,pz));
+    }
+
 
     void processKeyInput() {
         if(Gdx.input.isKeyPressed(Input.Keys.UP))
             arrowAction.doAction(Input.Keys.UP);
-        else if(Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
+        else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             arrowAction.doAction(Input.Keys.RIGHT);
         }
-        else if(Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
+        else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
             arrowAction.doAction(Input.Keys.DOWN);
         }
-        else if(Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
+        else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             arrowAction.doAction(Input.Keys.LEFT);
         }
     }

97fa1173482d6657c204234130dd49f612e2dc90
Piotr Zielinski <xenox40@gmail.com>
2019-04-28 21:49:40 +0200

    Added simple Game Screen
    AB#30

--- core/src/com/geometric/wars/screens/GameScreen.java
+++ core/src/com/geometric/wars/screens/GameScreen.java
@@ -1,18 +1,27 @@
 package com.geometric.wars.screens;
 
-import com.geometric.wars.Cube;
 import com.geometric.wars.GeometricWars;
+import com.geometric.wars.debug.axises.CoordinateAxises3D;
 
 public class GameScreen extends AbstractScreen {
+    CoordinateAxises3D axises;
+
     public GameScreen(GeometricWars game) {
         super(game);
+        axises = new CoordinateAxises3D();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
 
+        game.player.update();
+
         batch.begin(camera);
-        batch.end();;
+        axises.render(batch, environment);
+        batch.render(game.floor1, environment);
+        batch.render(game.floor2, environment);
+        game.player.render(batch, environment);
+        batch.end();
     }
 }

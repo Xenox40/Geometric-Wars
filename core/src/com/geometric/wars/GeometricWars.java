f96ff54a43f6806aa7e83479e8fbe6bdac0c8983
adam furtak <adam.furtak@outlook.com>
2019-04-29 01:06:34 +0200

    added choosing input controller depens on platform and make arrow input controller singleton

--- core/src/com/geometric/wars/GeometricWars.java
+++ core/src/com/geometric/wars/GeometricWars.java
@@ -1,8 +1,12 @@
 package com.geometric.wars;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.geometric.wars.enviromentparts.Floor;
 import com.geometric.wars.input.ArrowInputController;
+import com.geometric.wars.input.InputController;
+import com.geometric.wars.input.swipe.SwipeInputController;
 import com.geometric.wars.models.CubeModel;
 import com.geometric.wars.models.FloorModel;
 import com.geometric.wars.models.WallModel;
@@ -18,14 +22,20 @@ public class GeometricWars extends ApplicationAdapter {
 	public Floor floor2;
 	private GameScreen gameScreen;
 
+	private InputController inputController;
+
 	@Override
 	public void create () {
 		gameScreen = new GameScreen(this);
 
-		player = new ShooterPlayersController(0, 0, new PersonsCubeFactory(new ArrowInputController()));
+		if (Gdx.app.getType() == Application.ApplicationType.Android)
+			inputController = SwipeInputController.getInstance();
+		else
+			inputController = ArrowInputController.getInstance();
+
+		player = new ShooterPlayersController(0, 0, new PersonsCubeFactory(inputController));
 		floor1 = new Floor(0, 0);
 		floor2 = new Floor(1, 0);
-
 	}
 
 	@Override

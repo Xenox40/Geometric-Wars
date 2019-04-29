d15dec8ed038455bf4ec0cdab54498dc94834bf5
Piotr Zielinski <xenox40@gmail.com>
2019-04-29 02:50:28 +0200

    Game screen renders a given map
    AB#39

--- core/src/com/geometric/wars/GeometricWars.java
+++ core/src/com/geometric/wars/GeometricWars.java
@@ -3,39 +3,27 @@ package com.geometric.wars;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.geometric.wars.enviromentparts.Floor;
 import com.geometric.wars.input.ArrowInputController;
 import com.geometric.wars.input.InputController;
 import com.geometric.wars.input.swipe.SwipeInputController;
 import com.geometric.wars.models.CubeModel;
 import com.geometric.wars.models.FloorModel;
 import com.geometric.wars.models.WallModel;
-import com.geometric.wars.player.ShooterPlayersController;
-import com.geometric.wars.player.person.PersonsCubeFactory;
 import com.geometric.wars.screens.GameScreen;
 
 
 public class GeometricWars extends ApplicationAdapter {
-	//TODO getters
-	public ShooterPlayersController player;
-	public Floor floor1;
-	public Floor floor2;
 	private GameScreen gameScreen;
 
 	private InputController inputController;
 
 	@Override
 	public void create () {
-		gameScreen = new GameScreen(this);
-
-		if (Gdx.app.getType() == Application.ApplicationType.Android)
-			inputController = SwipeInputController.getInstance();
-		else
-			inputController = ArrowInputController.getInstance();
-
-		player = new ShooterPlayersController(0, 0, new PersonsCubeFactory(inputController));
-		floor1 = new Floor(0, 0);
-		floor2 = new Floor(1, 0);
+		gameScreen = new GameScreen(this, "map1.txt");
+        if (Gdx.app.getType() == Application.ApplicationType.Android)
+            inputController = SwipeInputController.getInstance();
+        else
+            inputController = ArrowInputController.getInstance();
 	}
 
 	@Override
--- core/src/com/geometric/wars/screens/GameScreen.java
+++ core/src/com/geometric/wars/screens/GameScreen.java
@@ -2,26 +2,29 @@ package com.geometric.wars.screens;
 
 import com.geometric.wars.GeometricWars;
 import com.geometric.wars.debug.axises.CoordinateAxises3D;
+import com.geometric.wars.maps.Map;
+import com.geometric.wars.maps.MapLoader;
 
 public class GameScreen extends AbstractScreen {
     CoordinateAxises3D axises;
+    Map map;
 
-    public GameScreen(GeometricWars game) {
+    public GameScreen(GeometricWars game, String mapName) {
         super(game);
         axises = new CoordinateAxises3D();
+        map = new MapLoader().setFileName("maps/"+mapName).load();
+        System.out.println(new MapLoader().setFileName("maps/"+mapName).load().staticMapObjects.size);
+        System.out.println(map.staticMapObjects.size);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-
-        game.player.update();
+        map.update();
 
         batch.begin(camera);
+        map.render(batch, environment);
         axises.render(batch, environment);
-        batch.render(game.floor1, environment);
-        batch.render(game.floor2, environment);
-        game.player.render(batch, environment);
         batch.end();
     }
 }

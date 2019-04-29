8a68835b1e31a1c2637fb34c79b5b6fc3104df90
Piotr Zielinski <xenox40@gmail.com>
2019-04-29 02:46:12 +0200

    Environment parts implements StaticMapObject

--- core/src/com/geometric/wars/enviromentparts/Floor.java
+++ core/src/com/geometric/wars/enviromentparts/Floor.java
@@ -1,14 +1,17 @@
 package com.geometric.wars.enviromentparts;
 
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.math.Vector3;
 import com.geometric.wars.Values;
+import com.geometric.wars.maps.StaticMapObject;
 import com.geometric.wars.models.FloorModel;
 
 /**
  * Represents a floor as an not-interactive model instance.
  */
-public class Floor extends ModelInstance {
+public class Floor extends ModelInstance implements StaticMapObject {
 
     /**
      * Create new instance of Floor model, and
@@ -19,6 +22,11 @@ public class Floor extends ModelInstance {
      */
     public Floor(int x, int y) {
         //TODO constants for y cord
-        super(FloorModel.getModel(), new Vector3(x * Values.unit, -Values.unit/2-0.05f, y * Values.unit));
+        super(FloorModel.getModel(), new Vector3(x * Values.unit, -(float)(Values.unit) / 2 - 0.05f, y * Values.unit));
+    }
+
+    @Override
+    public void render(ModelBatch modelBatch, Environment environment) {
+        modelBatch.render(this, environment);
     }
 }

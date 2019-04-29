8375eedae26abe2fd90c4b489c2a5c709eec52c1
Piotr Zielinski <xenox40@gmail.com>
2019-04-29 02:47:49 +0200

    Added Map class with MapLoader
    AB#22 AB#23

---
+++ core/src/com/geometric/wars/maps/MapLoader.java
@@ -0,0 +1,78 @@
+package com.geometric.wars.maps;
+
+import com.geometric.wars.Values;
+import com.geometric.wars.enviromentparts.Floor;
+import com.geometric.wars.enviromentparts.Wall;
+import com.geometric.wars.input.ArrowInputController;
+import com.geometric.wars.player.ShooterPlayersController;
+import com.geometric.wars.player.bots.randomactingbot.RandomBotFactory;
+import com.geometric.wars.player.person.PersonsCubeFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MapLoader {
+    private String fileName;
+    private int width = 13;
+    private int height = 13;
+
+    public Map load() {
+        BufferedReader reader;
+        Map map = new Map();
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+            System.out.println(line);
+            int lineCounter = 0;
+            int x = 0, y = 0;
+            while(line != null) {
+                ++lineCounter;
+                if(line.length() != width)
+                    throw new IOException("Wrong map width in: "+fileName);
+
+                for (char item: line.toCharArray()) {
+                    map.staticMapObjects.add(new Floor(x, y));
+                    switch (item) {
+                        case '#':
+                            map.staticMapObjects.add(new Wall(x, y));
+                            break;
+                        case 'P':
+                            map.dynamicMapObjects.add(new ShooterPlayersController(x, y, new PersonsCubeFactory(new ArrowInputController())));
+                            break;
+                        case 'B':
+                            map.dynamicMapObjects.add(new ShooterPlayersController(x, y, new RandomBotFactory()));
+                            break;
+                    }
+                    x += Values.unit;
+                }
+
+                line = reader.readLine();
+                x = 0; y += Values.unit;
+            }
+            if(lineCounter != height)
+                throw new IOException("Wrong map height in: "+fileName);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(map.staticMapObjects.size);
+        return map;
+    }
+
+    public MapLoader setFileName(String fileName) {
+        this.fileName = fileName;
+        return this;
+    }
+
+    public MapLoader setWidth(int width) {
+        this.width = width;
+        return this;
+    }
+
+    public MapLoader setHeight(int height) {
+        this.height = height;
+        return this;
+    }
+
+}

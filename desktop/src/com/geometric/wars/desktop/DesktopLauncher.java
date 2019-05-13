88dbfd3258e2bca496b76a04a933cf3e295bcc67
Piotr Zielinski <xenox40@gmail.com>
2019-05-13 05:02:04 +0200

    Adjusted resolution for PC

--- desktop/src/com/geometric/wars/desktop/DesktopLauncher.java
+++ desktop/src/com/geometric/wars/desktop/DesktopLauncher.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.geometric.wars.GeometricWars;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 720;
+		config.width = 1280;
 		new LwjglApplication(new GeometricWars(), config);
 	}
 }

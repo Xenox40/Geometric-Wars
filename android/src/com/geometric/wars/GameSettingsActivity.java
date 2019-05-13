9bbf0d58ecd26341749405712b2d47ebc0f2537c
adam furtak <adam.furtak@outlook.com>
2019-05-13 04:00:16 +0200

    added map preview

--- android/src/com/geometric/wars/GameSettingsActivity.java
+++ android/src/com/geometric/wars/GameSettingsActivity.java
@@ -2,9 +2,14 @@ package com.geometric.wars;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import com.geometric.mapgenerators.CornerMapPlayerPlacer;
 import com.geometric.mapgenerators.DefaultMapGenerator;
 import com.geometric.mapgenerators.GameMap;
@@ -13,16 +18,17 @@ public class GameSettingsActivity extends Activity {
 
     private Button playButton;
     private Button generateMap;
+    private ImageView previewImage;
 
     private DefaultMapGenerator mapGenerator = new DefaultMapGenerator(2, .30f);
-    private CornerMapPlayerPlacer playerPlacer = new CornerMapPlayerPlacer(1, 1);
+    private CornerMapPlayerPlacer playerPlacer = new CornerMapPlayerPlacer(3, 3);
     private GameMap map;
 
     @Override
     protected void onCreate(Bundle savedStateInstance) {
         super.onCreate(savedStateInstance);
         setContentView(R.layout.game_settings_activity);
-
+        previewImage = findViewById(R.id.preview_image);
         generateMap();
 
         playButton = findViewById(R.id.play_button);
@@ -40,11 +46,41 @@ public class GameSettingsActivity extends Activity {
                 generateMap();
             }
         });
+
+
     }
 
     private void generateMap() {
-        map = mapGenerator.generate(20, 20);
+        map = mapGenerator.generate(10, 10);
         playerPlacer.place(map);
+
+        updatePreview();
+    }
+
+    private void updatePreview() {
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
+        Bitmap bmp = Bitmap.createBitmap(map.getWidth(), map.getHeight(), conf); // this creates a MUTABLE bitmap
+
+        for (int i = 0; i < map.getHeight(); i++) {
+            for (int j = 0; j < map.getWidth(); j++) {
+                if (map.get(j, i) == '#')
+                    bmp.setPixel(j, i, Color.argb(255, 0, 255, 0));
+                if (map.get(j, i) == 'P')
+                    bmp.setPixel(j, i, Color.argb(255, 255, 0, 0));
+                if (map.get(j, i) == 'B')
+                    bmp.setPixel(j, i, Color.argb(255, 0, 0, 255));
+                if (map.get(j, i) == '.')
+                    bmp.setPixel(j, i, Color.argb(255, 0, 0, 0));
+            }
+        }
+
+        BitmapDrawable bd = new BitmapDrawable(this.getResources(), bmp);
+        bd.setFilterBitmap(false);
+
+        previewImage.setImageDrawable(bd);
+
+
+
     }
 
     private void openGameActivity() {

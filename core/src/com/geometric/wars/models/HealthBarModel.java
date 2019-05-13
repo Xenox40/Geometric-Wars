ddadd9b770ed1b35c017abdea2c2100c622fc548
Piotr Zielinski <xenox40@gmail.com>
2019-05-13 06:50:35 +0200

    Renamed HealthBarModel

--- core/src/com/geometric/wars/models/HealthBarModel.java
+++ core/src/com/geometric/wars/models/HealthBarModel.java
@@ -1,14 +1,14 @@
-package com.geometric.wars.gameobjects;
+package com.geometric.wars.models;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 
-public class HealthBar {
+public class HealthBarModel {
     private Texture texture;
     private NinePatch bar;
 
-    public HealthBar() {
+    public HealthBarModel() {
         texture = new Texture(Gdx.files.internal("healthBar.jpg"));
         bar = new NinePatch(texture, 0, 0, 0, 0);
     }

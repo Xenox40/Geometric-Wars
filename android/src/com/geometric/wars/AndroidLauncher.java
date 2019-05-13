4010351a8792bb4800269c04666ae433338df83c
adam furtak <adam.furtak@outlook.com>
2019-05-13 02:44:00 +0200

    rewrite menu

--- android/src/com/geometric/wars/AndroidLauncher.java
+++ android/src/com/geometric/wars/AndroidLauncher.java
@@ -5,12 +5,22 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 public class AndroidLauncher extends Activity {
 
 	private Button playButton;
+	private Button customGameButton;
+	private Button aboutButton;
+	private Button howToPlayButton;
+	private Button exitButton;
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
@@ -18,6 +28,10 @@ public class AndroidLauncher extends Activity {
 		setContentView(R.layout.menu_layout);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+		initializeButtons();
+	}
+
+	private void initializeButtons() {
 		playButton = findViewById(R.id.play_button);
 		playButton.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -25,16 +39,83 @@ public class AndroidLauncher extends Activity {
 				openGameActivity();
 			}
 		});
+
+		customGameButton = findViewById(R.id.custom_game_button);
+		customGameButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				openGameSettingsActivity();
+			}
+		});
+
+		aboutButton = findViewById(R.id.about_button);
+		aboutButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				openAboutActivity();
+			}
+		});
+
+		howToPlayButton = findViewById(R.id.how_to_play_button);
+		howToPlayButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				openHowToPlayButton();
+			}
+		});
+
+		exitButton = findViewById(R.id.exit_button);
+		exitButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				finish();
+				System.exit(0);
+			}
+		});
 	}
 
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		super.onConfigurationChanged(newConfig);
+	private void openHowToPlayButton() {
+		openPopupWindow("How to play?", "todo how to play");
+	}
 
+	private void openAboutActivity() {
+		openPopupWindow("About the application", "todo about");
+
+	}
+
+	private void openGameSettingsActivity() {
+		Intent intent = new Intent(this, GameSettingsActivity.class);
+		startActivity(intent);
 	}
 
 	private void openGameActivity() {
 		Intent intent = new Intent(this, GameActivity.class);
 		startActivity(intent);
+	}
+
+	private void openPopupWindow(String title, String content) {
+		LayoutInflater inflater = (LayoutInflater)
+				getSystemService(LAYOUT_INFLATER_SERVICE);
+		View popupView = inflater.inflate(R.layout.popup_window, null);
+
+		int width = LinearLayout.LayoutParams.MATCH_PARENT;
+		int height = LinearLayout.LayoutParams.MATCH_PARENT;
+		final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
+
+		popupWindow.showAtLocation(playButton, Gravity.CENTER, 0, 0);
+
+		popupView.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				popupWindow.dismiss();
+				return true;
+			}
+		});
+
+		TextView titleTv = popupView.findViewById(R.id.title);
+		TextView contentTv = popupView.findViewById(R.id.content);
+
+		titleTv.setText(title);
+		contentTv.setText(content);
 	}
 }
---
+++ android/src/com/geometric/wars/GameSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.geometric.wars;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameSettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedStateInstance) {
+        super.onCreate(savedStateInstance);
+        setContentView(R.layout.game_settings_activity);
+    }
+}

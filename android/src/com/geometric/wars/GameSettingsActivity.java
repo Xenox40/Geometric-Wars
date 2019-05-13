d64b757727fbdb2f518ef1fbc6b419ce7208b60e
adam furtak <adam.furtak@outlook.com>
2019-05-13 04:36:45 +0200

    added settings to map generation in menu

--- android/src/com/geometric/wars/GameSettingsActivity.java
+++ android/src/com/geometric/wars/GameSettingsActivity.java
@@ -3,13 +3,13 @@ package com.geometric.wars;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.*;
 import com.geometric.mapgenerators.CornerMapPlayerPlacer;
 import com.geometric.mapgenerators.DefaultMapGenerator;
 import com.geometric.mapgenerators.GameMap;
@@ -19,8 +19,17 @@ public class GameSettingsActivity extends Activity {
     private Button playButton;
     private Button generateMap;
     private ImageView previewImage;
+    private Button mapSettings;
 
-    private DefaultMapGenerator mapGenerator = new DefaultMapGenerator(2, .30f);
+    SeekBar widthSlider;
+    SeekBar heightSlider;
+    SeekBar wallThresholdSlider;
+
+    private int width = 10;
+    private int height = 10;
+    private float wallThreshold = .5f;
+
+    private DefaultMapGenerator mapGenerator = new DefaultMapGenerator(2, wallThreshold);
     private CornerMapPlayerPlacer playerPlacer = new CornerMapPlayerPlacer(3, 3);
     private GameMap map;
 
@@ -47,11 +56,19 @@ public class GameSettingsActivity extends Activity {
             }
         });
 
+        mapSettings = findViewById(R.id.map_settings);
+        mapSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSettingsPopup();
+            }
+        });
+
 
     }
 
     private void generateMap() {
-        map = mapGenerator.generate(10, 10);
+        map = mapGenerator.generate(width, height);
         playerPlacer.place(map);
 
         updatePreview();
@@ -63,13 +80,13 @@ public class GameSettingsActivity extends Activity {
 
         for (int i = 0; i < map.getHeight(); i++) {
             for (int j = 0; j < map.getWidth(); j++) {
-                if (map.get(j, i) == '#')
+                if (map.get(i, j) == '#')
                     bmp.setPixel(j, i, Color.argb(255, 0, 255, 0));
-                if (map.get(j, i) == 'P')
+                if (map.get(i, j) == 'P')
                     bmp.setPixel(j, i, Color.argb(255, 255, 0, 0));
-                if (map.get(j, i) == 'B')
+                if (map.get(i, j) == 'B')
                     bmp.setPixel(j, i, Color.argb(255, 0, 0, 255));
-                if (map.get(j, i) == '.')
+                if (map.get(i, j) == '.')
                     bmp.setPixel(j, i, Color.argb(255, 0, 0, 0));
             }
         }
@@ -78,14 +95,44 @@ public class GameSettingsActivity extends Activity {
         bd.setFilterBitmap(false);
 
         previewImage.setImageDrawable(bd);
-
-
-
     }
 
     private void openGameActivity() {
         Intent intent = new Intent(this, GameActivity.class);
         intent.putExtra("MAP", map);
         startActivity(intent);
+    }
+
+    private void openSettingsPopup() {
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.settings_popup, null);
+
+        final int width = LinearLayout.LayoutParams.MATCH_PARENT;
+        int height = LinearLayout.LayoutParams.MATCH_PARENT;
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
+
+        popupWindow.showAtLocation(playButton, Gravity.CENTER, 0, 0);
+
+
+        widthSlider = popupView.findViewById(R.id.width_slider);
+        heightSlider = popupView.findViewById(R.id.height_slider);
+        wallThresholdSlider = popupView.findViewById(R.id.wall_threshold);
+
+        Button ok = popupView.findViewById(R.id.ok_button);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateThings();
+                popupWindow.dismiss();
+            }
+        });
+
+    }
+
+    private void updateThings() {
+        width = widthSlider.getProgress() + 5;
+        height = heightSlider.getProgress() + 5;
+        wallThreshold = wallThresholdSlider.getProgress() / 100;
     }
 }

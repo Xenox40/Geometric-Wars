e7ebac4c1c6768b343a698c6d9214b94bbd878d5
Piotr Zielinski <xenox40@gmail.com>
2019-05-13 05:01:48 +0200

    SplashScreen is now shown for 2 seconds

--- core/src/com/geometric/wars/GeometricWars.java
+++ core/src/com/geometric/wars/GeometricWars.java
@@ -8,47 +8,56 @@ import com.geometric.wars.input.InputController;
 import com.geometric.wars.input.swipe.SwipeInputController;
 import com.geometric.wars.models.*;
 import com.geometric.wars.screens.GameScreen;
+import com.geometric.wars.screens.SplashScreen;
 import com.geometric.wars.utils.TypeOfGame;
 
-
 public class GeometricWars extends ApplicationAdapter {
+	private SplashScreen splashScreen;
 	private GameScreen gameScreen;
-    private InputController inputController;
-    private TypeOfGame typeOfGame;
-    private String mapName;
+	private InputController inputController;
+	private TypeOfGame typeOfGame;
+	private String mapName;
+	private float gameTime;
 
-    public GeometricWars() {
-    	typeOfGame = TypeOfGame.MOCK_GAME;
+	public GeometricWars() {
+		typeOfGame = TypeOfGame.MOCK_GAME;
 		mapName = "map2.txt";
+		gameTime = 0f;
 	}
 
 	public GeometricWars(TypeOfGame typeOfGame, String mapName) {
 		this.typeOfGame = typeOfGame;
 		this.mapName = mapName;
+		this.gameTime = 0f;
 	}
 
 	@Override
-	public void create () {
-        if (Gdx.app.getType() == Application.ApplicationType.Android)
-            inputController = SwipeInputController.getInstance();
-        else
+	public void create() {
+		if (Gdx.app.getType() == Application.ApplicationType.Android)
+			inputController = SwipeInputController.getInstance();
+		else
 			inputController = ArrowInputController.getInstance();
 
+		splashScreen = new SplashScreen(this);
 		gameScreen = new GameScreen(this, mapName);
 	}
 
 	@Override
-	public void render () {
-		gameScreen.render(1/30f);
+	public void render() {
+		if (gameTime < 2f) {
+			gameTime += Gdx.graphics.getDeltaTime();
+			splashScreen.render(1 / 30f);
+		} else
+			gameScreen.render(1 / 30f);
 	}
 
 	@Override
-	public void resize(int x,int y){
-		gameScreen.resize(x,y);
+	public void resize(int x, int y) {
+		gameScreen.resize(x, y);
 	}
 
 	@Override
-	public void dispose () {
+	public void dispose() {
 		DynamicCubeModelDisposer.dispose();
 		FloorModel.dispose();
 		WallModel.dispose();
@@ -57,7 +66,7 @@ public class GeometricWars extends ApplicationAdapter {
 		inputController.dispose();
 	}
 
-    public InputController getInputController() {
-        return inputController;
-    }
+	public InputController getInputController() {
+		return inputController;
+	}
 }

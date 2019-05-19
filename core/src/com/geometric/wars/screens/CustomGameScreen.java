58c68370cdd8476777098ea4684ebbf91653c45f
ggawryal <ggawryal@gmail.com>
2019-05-20 00:17:52 +0200

    added custom game menu

--- core/src/com/geometric/wars/screens/CustomGameScreen.java
+++ core/src/com/geometric/wars/screens/CustomGameScreen.java
@@ -1,25 +1,46 @@
 package com.geometric.wars.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Array;
+import com.geometric.mapgenerators.*;
 import com.geometric.wars.GeometricWars;
+import com.geometric.wars.maps.GameMap;
+
 
 public class CustomGameScreen extends AbstractMenuScreen{
+    private static final float mapPreviewWidth = 600, mapPreviewHeight = 600;
+
+    private int width = 13;
+    private int height = 13;
+    private float wallThreshold = .5f;
+
+    MapBuilder builder = new MapBuilder();
+    private GameMap map;
+
+    private Array<Array<Image> > images;
+    private Texture texture;
 
     public CustomGameScreen(GeometricWars game) {
         super(game);
-
+        texture = new Texture(Gdx.files.internal("mapPreviewTile.png"));
     }
 
     @Override
     public void show() {
         super.show();
+        generateMap();
+
+
         Table table = new Table();
         table.setFillParent(true);
         table.top();
-
         TextButton playButton = new TextButton("Play!", skin);
         TextButton customGameButton = new TextButton("Map settings", skin);
         TextButton backButton = new TextButton("Back", skin);
@@ -28,6 +49,7 @@ public class CustomGameScreen extends AbstractMenuScreen{
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(game.splashScreen);
+                game.gameScreen.setMap(map);
                 game.splashScreen.setNextScreen(game.gameScreen);
             }
         });
@@ -38,13 +60,62 @@ public class CustomGameScreen extends AbstractMenuScreen{
             }
         });
 
-        table.add(playButton).minSize(500,80).expand();
-        table.row();
-        table.add(customGameButton).minSize(500,80).expand();
-        table.row();
-        table.add(backButton).minSize(500,80).expand();
+        Table buttonsTable = new Table();
+        buttonsTable.top();
+        buttonsTable.add(playButton).minSize(300,60).expand();
+        buttonsTable.row();
+        buttonsTable.add(customGameButton).minSize(300,60).expand();
+        buttonsTable.row();
+        buttonsTable.add(backButton).minSize(300,60).expand();
+        buttonsTable.row();
 
+
+        Table imageTable = new Table();
+        imageTable.top();
+
+        int cellSize = (int)(mapPreviewWidth/map.getWidth() < mapPreviewHeight/map.getHeight() ? mapPreviewWidth/map.getWidth() : mapPreviewHeight/map.getHeight());
+
+        for(int i=0;i<map.getHeight();i++) {
+            for(int j=0;j<map.getHeight();j++)
+                imageTable.add(images.get(i).get(j)).minSize(cellSize,cellSize);
+            imageTable.row();
+        }
+
+        table.add(buttonsTable).minSize(200,600).expand();
+        table.add(imageTable).minSize(mapPreviewWidth,mapPreviewHeight).expand();
         stage.addActor(table);
+    }
+
+    private void generateMap() {
+        builder.setGenerator(new DefaultMapGenerator(15,wallThreshold)).setCompressor(new CuttingMapSizeCompressor(),3,3).setConnector(new DefaultMapConnector(5)).setPlayerPlacer(new CornerMapPlayerPlacer(4,3));
+        map = builder.build(width,height);
+        updatePreview();
+    }
+
+    private void updatePreview() {
+        if(images == null)
+            images = new Array<>();
+        images.clear();
+        for(int i=0;i<map.getHeight();i++){
+            images.add(new Array<Image>());
+            for(int j=0;j<map.getWidth();j++){
+                images.get(i).add(new Image(texture));
+                if (map.get(i, j) == '#')
+                    images.get(i).get(j).setColor(Color.LIME);
+                if (map.get(i, j) == 'P')
+                    images.get(i).get(j).setColor(Color.RED);
+                if (map.get(i, j) == 'B')
+                    images.get(i).get(j).setColor(Color.BLUE);
+                if (map.get(i, j) == '.')
+                    images.get(i).get(j).setColor(Color.BLACK);
+            }
+        }
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        texture.dispose();
     }
 
 
